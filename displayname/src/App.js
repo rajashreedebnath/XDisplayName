9039ef74a5f3757933ffe5d0bedc89205bf77bbd
rajashreedebnath <rajashreedebnath29@gmail.com>
2024-04-29 03:26:32 +0530

    Test

--- displayname/src/App.js
+++ displayname/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import react, { useState } from "react";
 import './App.css';
 
-function App() {
+export default function DisplayName() {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [formError, setFormError] = useState('');
+
+
+  
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+
+    if(firstName.trim() !== '' && lastName.trim() !== '') {
+
+      setFullName(`Full Name: ${firstName} ${lastName}`);
+      setFormError('');
+    }
+
+    else {
+      setFullName('');
+      setFormError('Please fill out this field');
+    }
+
+  };
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="display">
+      
+      <h1>Full Name Display</h1>
+
+      <form onSubmit={handleSubmit}>
+
+        <div className="firstName">
+
+          <label htmlFor="firstName">First Name: </label>
+          <input type="text" name="firstName" id="firstName" value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+
+        </div>
+
+        <div className="lastName">
+
+          <label htmlFor="lastName">Last Name: </label>
+          <input type="text" name="lastName" id="lastName" value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+
+        </div>
+
+        <button type="submit" className="submit"> Submit </button>
+
+        {formError && (
+          <div>{formError}</div>
+        )}
+
+      </form>
+
+
+
+      {fullName && (
+        <div>{fullName}</div>
+      )}
+
+
+
     </div>
   );
 }
-
-export default App;
